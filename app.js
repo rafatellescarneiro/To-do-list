40a50be425a090804d2d6a493d317fa66a92046b
rafatellescarneiro <rafatellescarneiro@gmail.com>
2022-04-08 12:03:43 -0300

    commit remove checked sucess

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 
 const campo = document.getElementById('nome-prod');
 const btnAdd = document.getElementById('botao-add');
+const clearBtn = document.getElementById('botao-del');
 const label = document.getElementById('lista');
-
+const rmvCheck = document.getElementById('del-check')
+const checkbox = document.getElementById('checkbox');
 
 let lista = [];
 
@@ -25,21 +27,24 @@ function removeItem(id) {
     }
   })
   lista = novaLista;
+  saveStorage();
   updateScreen();
 }
 
-
 function updateScreen(status, indice){
     label.innerHTML = '';
+   
 
     lista.forEach(function(item){
     const item1 = document.createElement('label');
+    item1.setAttribute('class', `tod-item ${item.id}`);
     item1.classList.add('lista')
     item1.id = `i${item.id}`;
     item1.innerHTML = `
-      <input type="checkbox" ${status} data-indice=${indice}>
-      <div class="divmon">${item.name}</div>
+      <input type="checkbox" ${status} data-indice=${indice} id="checkbox">
+      <span class="divmon"id="divmon">${item.name}</span>
     `;
+
     const btn = document.createElement('button');
     btn.innerHTML = 'x';
     btn.onclick = function () {
@@ -47,23 +52,24 @@ function updateScreen(status, indice){
     }
     item1.appendChild(btn);
     document.getElementById('lista').appendChild(item1)
-    });
-  }
+    });   
 
-  
+    
+}
 
-  function saveStorage() {
+function saveStorage() {
     
     const listaJSON = JSON.stringify(lista);
     localStorage.setItem('lista', listaJSON);
-  }
+}
 
-  function addItem() {
+function addItem() {
     
     if (campo.value) {
       
       lista.push({
         id: Date.now(),
+        checked: false,
         name: campo.value
       });
       
@@ -73,11 +79,37 @@ function updateScreen(status, indice){
     } else {
       alert('Insira o nome de um item!');
     }
-  }
+}
 
+function clearList(item){
+  var limpar = confirm('Deseja limpar toda lista?')
+    if(limpar === true){
+          lista.splice(item);
+        }
+    saveStorage();
+    updateScreen();
+  } 
+
+function removeCheck(){
+    var doc = document.querySelectorAll('.lista1');
+    doc.forEach(item => {
+     if(item.querySelector('input').checked){
+       item.remove()
+     }
+    })
+    
+    saveStorage();
+
+}
+
+
+
+ 
 
 btnAdd.addEventListener('click', addItem);
-
+clearBtn.addEventListener('click', clearList);
+rmvCheck.addEventListener('click', removeCheck)
+  
 
 campo.addEventListener('keydown', function (event) {
   
